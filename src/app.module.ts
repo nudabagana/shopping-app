8a63430730d4b30b9d77bcd34aedbcabc8e68e01
Matas Ramanauskas <matasramanauskas@gmail.com>
2023-02-10 22:18:25 +0200

    typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsController } from './controllers/products.controller';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+if (!process.env.DB_USER || !process.env.DB_PASSWORD) {
+  throw new Error(
+    'Env variables DB_USER or DB_PASSWORD not set. Please upadate your .env file. ',
+  );
+}
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: 'shoppingdb',
+      entities: [],
+    }),
+  ],
   controllers: [ProductsController],
   providers: [],
 })
